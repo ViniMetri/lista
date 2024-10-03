5bab1c3c55feef00d0af15f752114d970e95ec8a
Vinícius Alves <vinicius.metri@gmail.com>
2024-10-03 20:52:01 -0300

    segundo commit

--- autenticador/controlar_usuarios (questao05).py
+++ autenticador/controlar_usuarios (questao05).py
@@ -1,8 +1,21 @@
-def autenticar(login):
-    usuarios = [
+def autenticar(login, senha):
+    for usuario in usuarios:
+        if login == usuario['username']:
+            
+            for teste_senha in usuarios:
+                if senha == teste_senha['password']:
+                    print("Acesso Liberado")
+                    break
+                else:
+                    print("Acesso Negado")
+                    
+        else:
+            print("Acesso Negado")
+        
+usuarios = [
         {
-        "username" : "teste",
-        "password" : "admin"  
+            "username" : "teste",
+            "password" : "admin"  
         },
         {
             "username" : "teste2",
@@ -12,10 +25,8 @@ def autenticar(login):
             "username" : "teste3",
             "password" : "admin3"
         }
-                ]
-    if login in usuarios:
-        print("oi")
+    ]
     
 login = input("Digite seu login: ")
 senha = input("Digite sua senha: ")
-autenticar(login)
+autenticar(login, senha)
